c654b3c0a4b3ddf50d732c3b633e6cdca575968f
naddanki08 <142543472+naddanki08@users.noreply.github.com>
2023-11-25 01:43:25 +0530

    Update GaussianElimination.java

--- GaussianElimination.java
+++ GaussianElimination.java
@@ -1,8 +1,10 @@
 
-public class GaussianElimination {
-
-    public static void main(String[] args) {
+public static void main(String[] args) {
+        // Define the size of the system of equations
         int n = 9;
+
+        // Define the coefficient matrix augmented with the right-hand side values
+
         double[][] coefficients = {
                 {1, 1, 1, 1, 1, 1, 1, 1, 1, 122},
                 {1, 1, 1, 1, 1, -1, -1, -1, -1, -88},
@@ -15,6 +17,7 @@ public class GaussianElimination {
                 {9, -8, 7, -6, 5, -4, 3, -2, 1, 41}
         };
 
+        // Initialize matrices for lower triangular and upper triangular forms
         double[][] lowerTriangularMatrix = new double[n][n + 1];
         double[][] upperTriangularMatrix = solveLinearSystem(coefficients, n, lowerTriangularMatrix);
 
@@ -38,18 +41,22 @@ public class GaussianElimination {
         printDiagonal(solutionMatrix);
     }
 
+    // Function to solve the linear system using Gaussian elimination with partial pivoting
     public static double[][] solveLinearSystem(double[][] coefficients, int n, double[][] lowerTriangularMatrix) {
+        // Initialize the augmented matrix
         double[][] augmentedMatrix = new double[n][n + 1];
 
         // Copy coefficients to augmented matrix
         for (int i = 0; i < n; i++) {
-            System.arraycopy(coefficients[i], 0, augmentedMatrix[i], 0, n + 1);
+            for (int j = 0; j <= n; j++) {
+                augmentedMatrix[i][j] = coefficients[i][j];
+            }
         }
 
         // Perform Gaussian elimination with partial pivoting
         for (int i = 0; i < n - 1; i++) {
+            // Find the pivot row
             int pivotRow = i;
-
             for (int j = i + 1; j < n; j++) {
                 if (Math.abs(augmentedMatrix[j][i]) > Math.abs(augmentedMatrix[pivotRow][i])) {
                     pivotRow = j;
@@ -60,10 +67,9 @@ public class GaussianElimination {
             swapRows(augmentedMatrix, i, pivotRow);
             swapRows(lowerTriangularMatrix, i, pivotRow);
 
+            // Update the lower triangular matrix and perform row operations
             for (int j = i + 1; j < n; j++) {
                 double ratio = augmentedMatrix[j][i] / augmentedMatrix[i][i];
-
-                // Update the lower triangular matrix
                 lowerTriangularMatrix[j][i] = ratio;
 
                 for (int k = i; k <= n; k++) {
@@ -80,7 +86,9 @@ public class GaussianElimination {
         return augmentedMatrix;
     }
 
+    // Function to solve both upper and lower triangular matrices
     public static double[][] solveBothMatrices(double[][] upperMatrix, double[][] lowerMatrix, int n) {
+        // Initialize the solution matrix
         double[][] solutionMatrix = new double[n][n + 1];
 
         // Solve upper triangular matrix
@@ -105,9 +113,12 @@ public class GaussianElimination {
         return solutionMatrix;
     }
 
+    // Function for back-substitution
     public static double[] backSubstitution(double[][] augmentedMatrix, int n) {
+        // Initialize the solution array
         double[] solution = new double[n];
 
+        // Iterate through rows in reverse order
         for (int i = n - 1; i >= 0; i--) {
             solution[i] = augmentedMatrix[i][n];
 
@@ -115,6 +126,7 @@ public class GaussianElimination {
                 solution[i] -= augmentedMatrix[i][j] * solution[j];
             }
 
+            // Avoid divide by zero
             double pivot = augmentedMatrix[i][i];
             if (pivot != 0) {
                 solution[i] /= pivot;
@@ -124,12 +136,14 @@ public class GaussianElimination {
         return solution;
     }
 
+    // Function to swap two rows in a matrix
     public static void swapRows(double[][] matrix, int row1, int row2) {
         double[] temp = matrix[row1];
         matrix[row1] = matrix[row2];
         matrix[row2] = temp;
     }
 
+    // Function to print a matrix
     public static void printMatrix(double[][] matrix) {
         for (double[] row : matrix) {
             for (double value : row) {
@@ -139,6 +153,7 @@ public class GaussianElimination {
         }
     }
 
+    // Function to print the diagonal elements of the matrix as x1, x2, ..., x9
     public static void printDiagonal(double[][] matrix) {
         for (int i = 0; i < matrix.length; i++) {
             System.out.println("x" + (i + 1) + " = " + matrix[i][i]);
